eb58c106b2a6819304a224ba3824a6eb89a57b5a
akatsukinoyami <akatsukinoyami@yahoo.com>
2021-04-20 02:02:27 +0300

    ver 0.8 small file replacements

--- routes.py
+++ routes.py
@@ -13,50 +13,41 @@ def decorator(func):
 			return render_template('404.html')
 	return wrapper
 
+def open_json(file):
+	with open(file, 'r') as fp:
+		return json.load(fp)
+
 def render_table(name):
-	with open(f'static/json/{name}.json', 'r') as content:
-		mydict = dict(json.load(content))
-		return render_template('sub/table.html', **mydict)
+	mydict = dict(open_json(f'static/json/{name}.json'))
+	return render_template('sub/table.html', **mydict)
 
 def render_list(address, template):
 	charlist = {}
-	for char in os.listdir(address):
+	for char in os.listdir(f'static/json/{address}'):
 		char = char.replace('.json', '')
-		with open(f'{address}/{char}.json', 'r') as file:
-				charlist[char] = json.load(file)
+		charlist[char] = open_json(f'static/json/{address}/{char}.json')
 	return render_template(template, chars=charlist)
 
 @decorator
-def int_index(_):
-	return render_table('index')
-
+def int_index(_):			return render_table('index')
 @decorator
-def int_info(_):
-    return render_table('info')
-
+def int_info(_):    	return render_table('info')
 @decorator
-def int_char_list(_):
-	return render_list('static/chars/info', 'charlist.html')
-
+def int_char_list(_):	return render_list('chars', 'charlist.html')
 @decorator
-def int_enemy_list(_):
-	return render_list('static/json/enemies', 'enemylist.html')
+def int_enemy_list(_):return render_list('enemies', 'enemylist.html')
 
 @decorator
 def int_user_profile(char):
-	with open(f'static/chars/info/{char}.json', 'r') as char_form:
-		kwargs = {
-			'template_name_or_list' : 'character.html',
-			'user' 									: json.load(char_form),
-			'link' 									: char,
-			'stats'									: ["никак", "слабо",	"обычно",	
-				"лучше среднего",	"хорошо",	"легко и умело","удивительно", 
-				"блестяще",	"прославленно", "легендарно"]}
+	kwargs = {
+		'template_name_or_list' : 'character.html',
+		'user' 									: open_json(f'static/json/chars/{char}.json'),
+		'link' 									: char,
+		'stats'									: ["никак", "слабо",	"обычно",	
+			"лучше среднего",	"хорошо",	"легко и умело","удивительно", 
+			"блестяще",	"прославленно", "легендарно"]}
 	return render_template(**kwargs)
 
 @decorator
 def int_get_user_profile_json(char):
-	with open(f'static/chars/info/{char}.json', 'r') as char_form:
-		return json.load(char_form)
-
-
+	return open_json(f'static/json/chars/{char}.json')
